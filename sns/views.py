2b2d096640fd6790e5d443a4e08506e52a4f9329
lazyarea <djangocube+tutorial@gmail.com>
2019-05-01 02:41:32 +0000

    first commit

--- sns/views.py
+++ sns/views.py
@@ -4,4 +4,36 @@ from django.contrib import messages
 
 from .models import Message,Friend,Group,Good
 from .forms import GroupCheckForm,GroupSelectForm,SearchForm,CreateGroupForm,PostForm
+
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url='/admin/login/')
+def index(request):
+
+    pass
+
+def groups(request):
+
+    pass
+
+def add(request):
+
+    pass
+
+def post(request):
+
+    pass
+
+def creategroup(request):
+
+    pass
+
+def share(request):
+
+    pass
+
+def good(request):
+
+    pass
