27ec59f2847a3e8ba54d4928ad474b5ae23a1dd1
vagrant <vagrant@localhost.localdomain>
2019-05-01 00:12:13 +0000

    next views.py

--- sns/views.py
+++ sns/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
+from .models import Message,Friend,Group,Good
+from .forms import GroupCheckForm,GroupSelectForm,SearchForm,CreateGroupForm,PostForm
 # Create your views here.
